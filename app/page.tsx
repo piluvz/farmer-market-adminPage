390be71a5500b277f9116987a5f160b16261e38f
piluvz <dilnaz.rakhmankulova@gmail.com>
2024-11-25 00:02:59 +0500

    upd redirect on main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-// import Image from "next/image";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+const HomePage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    
+    if (token) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  }, [router]);
+
   return (
-    <h1>home</h1>
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <p className="text-lg text-gray-700 animate-pulse">Redirecting...</p>
+    </div>
   );
-}
+};
+
+export default HomePage;
